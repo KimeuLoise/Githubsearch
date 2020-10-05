42544e490576c9dc1dfc2661eb635dfafd75221e
KimeuLoise <kimeuloise@gmail.com>
2020-10-05 23:00:30 +0300

    content addition in component ts

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Repo } from '../repo';
+import { SearchUserService } from '../search-user-services/search-user.service';
+import { SearchRepoService } from '../search-repo-services/search-repo.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-github',
@@ -6,10 +12,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+  users: User[];
+  repos: Repo[];
+  usersLength: number;
+  reposLength: number;
 
-  constructor() { }
+  constructor(
+    private searchUserService: SearchUserService,
+    private searchRepoService: SearchRepoService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  goToUrl(id) {
+    this.router.navigate(['/users', id]);
   }
 
+  search(term: string) {
+    this.searchUserService.fetchUsers(term).then(
+      () => {
+        this.users = this.searchUserService.usersFound;
+        this.usersLength = this.searchUserService.usersFound.length;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.searchRepoService.fetchRepos(term).then(
+      () => {
+        this.repos = this.searchRepoService.foundRepos;
+        this.reposLength = this.searchRepoService.foundRepos.length;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.search('Anne');
+  }
 }
+
